3a0b5f9925c5995b60421d4a46210d7934b7a145
HypherionSA <hypherionmc@gmail.com>
2025-07-01 12:59:15 +0200

    Fix compile error

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.util.Date
 
 plugins {
     id("java")
-    id("com.hypherionmc.modutils.orion") version "2.0.2"
+    id("com.hypherionmc.modutils.orion") version "2.0.3"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("xyz.wagyourtail.unimined") version "1.2.9"
     id("com.hypherionmc.modutils.modpublisher") version "2.1.6"
